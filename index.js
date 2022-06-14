10085130587e4d186afa6785cbfa8c14ece18aad
Jacob Rowland <jacob.rowland@hotmail.com>
2022-06-14 12:30:24 +1200

    refactored code

--- index.js
+++ index.js
@@ -4,77 +4,6 @@ const rwClient = require("./twitterClient.js");
 const fs = require('fs');
 var CronJob = require('cron').CronJob;
 
-async function captureImages() {
-    let paths = [];
-    const cameras = ["#tnLounge", "#tnDining", "#tnBedroom", "#tnGames", "#tnMusic", "#tnBedroom2"];
-    const edgeUrl = 'https://www.theedge.co.nz/home/win/2022/05/win--register-for-the-edge--50k-flatmate-with-dosh.html';
-
-    const browser = await puppeteer.launch({
-        executablePath: process.env.CHROME_PATH,
-        headless:false, 
-        defaultViewport:null,
-        devtools: true,
-        args: ["--window-size=1920,1080", "--window-position=1921,0"]
-      })
-
-    const page = await browser.newPage();
-    await page.goto(edgeUrl);
-    await page.waitForTimeout(10000);
-    
-    page.setViewport({width: 1920, height: 1080});
-
-    console.log("Loading page...")
-
-    const header = await page.$('#headerTitle');
-    console.log("Fetching title...");
-    let = title = '50k Flatmate Update';
-    try {
-      properties = await header.getProperty('innerHTML');
-      title = properties._remoteObject.value;
-    } catch(err) {
-      console.log(err);
-    }
-
-    for (let i = 0; i < cameras.length; i++) {
-      let camera = cameras[i];
-      console.log(`Capturing ${camera}`);
-      try {
-        await page.click(camera);
-        if (i !== 0) {
-          await page.click('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); // focus video player
-        }
-        await page.click('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); // unpause the video player
-        await page.waitForTimeout(5000);
-
-        let videoPlayer = await page.$('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); 
-        let path = `images/${camera}.png`;
-        await videoPlayer.screenshot({path: path});
-
-        paths.push(path);
-      }
-      catch {
-        console.log(`Failed to capture ${camera}`);
-      }
-    }
-    await browser.close();
-    return {paths: paths, title:title};
-  };
-
-const tweet = async(paths, title) => {
-  console.log("Posting images to Twitter...");
-  let selectedPaths = paths.sort(() => 0.5 - Math.random()).slice(0, 4);
-  const mediaIds = await Promise.all(selectedPaths.map(path => rwClient.v1.uploadMedia(`./${path}`)));
-  const date = getDateString();
-  await rwClient.v1.tweet(`${date}\n\n${title}\n\nWatch live @ http://theedge.co.nz`, { media_ids: mediaIds });
-
-  console.log("Cleaning up images...");
-  paths.forEach(path => {
-    fs.unlinkSync(path);
-  });
-
-  console.log("Upload complete.\n");
-};
-
 function getDateString() {
   let minutes = parseInt(new Date().getMinutes());
   if (minutes < 10) {
@@ -82,14 +11,91 @@ function getDateString() {
   }
   return `${new Date().toDateString()} ${new Date().getHours()}:${minutes}`;
 }
-  
-async function main() {
-  console.log(getDateString());
-  const {paths, title} = await captureImages();
-  console.log(title);
-  await tweet(paths, title);
+
+async function captureEachCamera(page) {
+  const cameras = ["#tnLounge", "#tnDining", "#tnBedroom", "#tnGames", "#tnMusic", "#tnBedroom2"];
+  let imagePaths = [];
+  for (let i = 0; i < cameras.length; i++) {
+    let camera = cameras[i];
+    console.log(`Capturing ${camera}`);
+    try {
+      await page.click(camera);
+      if (i !== 0) {
+        await page.click('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); // focus video player
+      }
+      await page.click('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); // unpause the video player
+      await page.waitForTimeout(5000);
+
+      let videoPlayer = await page.$('#player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video'); 
+      let path = `images/${camera}.png`;
+      await videoPlayer.screenshot({path: path});
+      imagePaths.push(path);
+    }
+    catch {
+      console.log(`Failed to capture ${camera}`);
+    }
+  }
+  return imagePaths;
 }
 
+async function getPageHeader(page) {
+  const header = await page.$('#headerTitle');
+  console.log("Fetching current stream header...");
+  let = title = 'Tune in to see the next challenge';
+  try {
+    properties = await header.getProperty('innerHTML');
+    title = properties._remoteObject.value;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+function deleteImages(paths) {
+  console.log("Deleting images...");
+  paths.forEach(path => {
+    fs.unlinkSync(path);
+  });
+}
+
+async function scrapePage() {
+    const edgeUrl = 'https://www.theedge.co.nz/home/win/2022/05/win--register-for-the-edge--50k-flatmate-with-dosh.html';
+    const browser = await puppeteer.launch({
+        executablePath: process.env.CHROME_PATH,
+        headless:true, 
+        defaultViewport:null,
+        devtools: false,
+        args: ["--window-size=1920,1080", "--window-position=1921,0"]
+      })
+    const page = await browser.newPage();
+    await page.goto(edgeUrl);
+    await page.waitForTimeout(10000);
+    page.setViewport({width: 1920, height: 1080});
+    console.log("Loading page...")
+    let title = await getPageHeader(page);
+    let paths = await captureEachCamera(page);
+    await browser.close();
+    return {paths: paths, title:title};
+  };
+
+const tweet = async(paths, title) => {
+  const body = `${date}\n\n${title}\n\nWatch live @ http://theedge.co.nz`;
+  const date = getDateString();
+  console.log("Posting images to Twitter...");
+  let selectedPaths = paths.sort(() => 0.5 - Math.random()).slice(0, 4); // shuffle paths and select 4 of the 6 images
+  const mediaIds = await Promise.all(selectedPaths.map(path => rwClient.v1.uploadMedia(`./${path}`))); // upload each image
+  await rwClient.v1.tweet(body, { media_ids: mediaIds }); // tweet and attach uploaded media
+
+  console.log("Upload complete.\n");
+};
+
+async function main() {
+  console.log(getDateString());
+  const {paths, title} = await scrapePage();
+  await tweet(paths, title);
+  deleteImages(paths);
+}
+
+// Run main() every hour
 const job = new CronJob("0 * * * *", () => {
   try {
     main();
@@ -100,5 +106,3 @@ const job = new CronJob("0 * * * *", () => {
 });
 
 job.start();
-
-main();
